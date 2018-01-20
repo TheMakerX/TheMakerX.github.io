f8186ead1e0a466fad515150691ae65b8b45619c
TheMakerX <35618171+TheMakerX@users.noreply.github.com>
2018-01-19 18:59:08 -0800

    Add files via upload

---
+++ HowTo_Ajay.js
@@ -0,0 +1,21 @@
+﻿	window.alert("This is a guide on how to cook a burger.")
+
+document.getElementById("defaultOpen").click();
+
+function openTab(tabName, elmt){
+//This javascript goes along with my How To project
+
+function openTab(tabName,elmt){
+	var i, tabcontent;
+	//Hide elements with class="tabcontent"
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < 3; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	
+	// Show the clicked tabName
+	document.getElementById(tabName).style.display = "block";
+	
+
+}
+document.getElementById("defaultOpen").click();
